3d07cdb63ccfb99cdf62fe7fb2d7db1d3903a7dd
JoshuaZe <joshua_zhang@foxmail.com>
2016-03-18 20:55:18 +0800

    new python code from learn python the hard way

--- example_2.py
+++ example_2.py
@@ -5,9 +5,22 @@ Created on Thu Mar 03 11:29:48 2016
 @author: Joshua
 """
 
-filename = "C:/Users/Joshua/Documents/Python Scripts/example_1.py"
+filename = "./example_1.py"
 
 txt = open(filename,"r")
 
 print "Here's your file %r:" % filename
 print txt.read()
+
+def print_all(f):
+    print f.read()
+
+print_all(txt)
+
+def rewind(f):
+    f.seek(0)
+rewind(txt)
+
+def print_a_line(line_count, f):
+    print line_count, f.readline()
+print_a_line(1,txt)
--- example_4.py
+++ example_4.py
@@ -5,3 +5,18 @@ Created on Thu Mar 03 11:40:47 2016
 @author: Joshua
 """
 
+# something like argv
+# That * tells Python to take all the arguments to 
+# the function and then put them in args as a list
+
+def print_two_A(*args):
+    #arg1,arg2=args
+    print "arg1: %r, arg2: %r" % (args)
+def print_two_B(*args):
+    arg1,arg2=args
+    print "arg1: %r, arg2: %r" % (arg1,arg2)
+def print_two_C(arg1,arg2):
+    print "arg1: %r, arg2: %r" % (arg1,arg2)
+
+print_two_A("hi","good")
+
