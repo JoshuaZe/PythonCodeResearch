9c9e04c395bfaacd6d913b22004fbca6e843deaf
JoshuaZe <joshua_zhang@foxmail.com>
2016-03-18 22:56:12 +0800

    more material reference

---
+++ example_15.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 18 22:29:03 2016
+http://learnpythonthehardway.org/book/ex48.html
+@author: Joshua
+"""
+
+first_word = ('verb', 'go')
+second_word = ('direction', 'north')
+third_word = ('direction', 'west')
+sentence = [first_word, second_word, third_word]
+
+def convert_number(s):
+    try:
+        return int(s)
+    except ValueError:
+        return None
+
+# http://learnpythonthehardway.org/book/ex50.html for web.py 
+
+# http://learnpythonthehardway.org/book/index.html
+
+# http://cli.learncodethehardway.org/bash_cheat_sheet.pdf
+# http://www.gnu.org/software/bash/manual/bashref.html
+
+
+# deep learning
+# numpy Matplotlib
+# http://www.scipy.org/
+# http://www.dexy.it/
+# http://pandas.pydata.org/
+# http://www.nltk.org/
+# http://scikit-learn.org/stable/
